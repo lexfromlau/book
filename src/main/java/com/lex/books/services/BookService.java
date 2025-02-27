c8a919a8d2a3e831461b1cb9cbb457625f782e2d
alex <alexander.polzhofer@gmail.com>
2025-02-27 23:18:11 +0100

    init project

---
+++ src/main/java/com/lex/books/services/BookService.java
@@ -0,0 +1,5 @@
+package com.lex.books.services;
+
+public interface BookService {
+
+}
---
+++ src/main/java/com/lex/books/services/impl/BookServiceImpl.java
@@ -0,0 +1,8 @@
+package com.lex.books.services.impl;
+
+import com.lex.books.services.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+}
